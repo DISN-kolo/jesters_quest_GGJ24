5db240bf74a55ec20b78536c8092c7b16ff8ec10
akozin <arrtemsky@gmail.com>
2024-01-26 19:20:10 +0100

    local changes + starting arrow

--- Characters/player.gd
+++ Characters/player.gd
@@ -1,28 +1,39 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var SPEED = 300.0
+@export var JUMP_VELOCITY = -400.0
+@export var down_vel_max = 400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var arrow_scene = preload("res://Projectile/arrow.tscn")
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		velocity.y = min(velocity.y, down_vel_max)
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	# Handle SHOOT >:)
+	if Input.is_action_just_pressed("LMB"):
+		shoot()
 	move_and_slide()
+
+func shoot():
+	var target = get_global_mouse_position()
+	var start = self.global_position
+	
+	
