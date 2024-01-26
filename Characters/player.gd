4be906ef529e4396ad67af77e61541372cc16d1a
akozin <arrtemsky@gmail.com>
2024-01-26 20:25:04 +0100

    grace period

--- Characters/player.gd
+++ Characters/player.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 
 
-@export var SPEED = 300.0
-@export var JUMP_VELOCITY = -400.0
+@export var SPEED = 200.0
+@export var JUMP_VELOCITY = -300.0
 @export var down_vel_max = 400.0
+
+# jump grace
+var floorer = 1.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -14,9 +17,12 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		velocity.y = min(velocity.y, down_vel_max)
+		floorer = lerp(floorer, 0.0, 40*delta)
+	else:
+		floorer = 1.0
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump")  and floorer > 0.001:
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
