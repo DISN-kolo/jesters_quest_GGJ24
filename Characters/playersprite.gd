f1d87808ec4596a5ab98ff4ce42575615cfa8ac2
akozin <arrtemsky@gmail.com>
2024-01-27 12:34:30 +0100

    payer sprite rotation FIXED FINALLY

--- Characters/playersprite.gd
+++ Characters/playersprite.gd
@@ -1,11 +1,16 @@
 extends Sprite2D
 
-
+var remember_offset : Vector2
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	remember_offset = self.offset
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if self.flip_h:
+		self.offset = remember_offset - Vector2(8, 0)
+	else:
+		self.offset = remember_offset
 	pass
