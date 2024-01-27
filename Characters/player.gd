1afad1f6bcc1d740b02b3cef5b460cd5b238df24
Molasz <martiolasz@gmail.com>
2024-01-27 16:35:41 +0100

    player death

--- Characters/player.gd
+++ Characters/player.gd
@@ -8,25 +8,27 @@ extends CharacterBody2D
 
 @export var SHOOT_CD = 0.1
 var shoot_cd = SHOOT_CD
-																				#Brian added some code
+
 @onready var animation = $AnimationPlayer
 @onready var sprite = $Sprite2D
+@onready var timer = $Timer
 
 var _facing_direction := -1
 var _was_moving := false
-																				#Brian added until here
+
+var HitEffect = preload("res://Effects/hit_effect.tscn")
+var DeathEffect = preload("res://Effects/enemy_death_effect.tscn")
 
 # jump grace
 var floorer = 1.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var arrow_scene = preload("res://Projectile/arrow.tscn")
 var random = RandomNumberGenerator.new()
 var arrows = ["res://Art Assets/ball_red.png", "res://Art Assets/ball_blue.png",
 "res://Art Assets/ball_green.png", "res://Art Assets/ball_yellow.png"]
 
-func _physics_process(delta):
+func _physics_process(delta):	
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		velocity.y = min(velocity.y, down_vel_max)
@@ -34,30 +36,23 @@ func _physics_process(delta):
 	else:
 		floorer = 1.0
 
-	# Handle jump.
 	if Input.is_action_just_pressed("jump")  and floorer > 0.001:
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
 	
 	if direction:
 		$AnimationPlayer.play("main char")
 		velocity.x = direction * SPEED
-																				#some more Brian's crap
 		_facing_direction = -1 if direction > 0 else 1
 		#animation.play("Run")
 		_was_moving = true
-																				#Brian is done with his bullshit coding for animations
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		$AnimationPlayer.play("stop")
-																				#okay, Brian needs some extra lines
 		_was_moving = false
 	if _was_moving:
 		sprite.flip_h = (_facing_direction < 0)
-																				#this is the end, Brian's promise
 	# Handle SHOOT >:)
 	if Input.is_action_pressed("LMB"):
 		if shoot_cd <= 0:
@@ -82,18 +77,35 @@ func shoot():
 	#arrow.speed = 2000
 	#arrow.target = target
 	owner.add_child(arrow)
-	
 
+func createHitEffect():
+	var hitEffect = HitEffect.instantiate()
+	hitEffect.global_position = global_position
+	get_parent().add_child(hitEffect)
+	
+func createDeathEffect():
+	var deathEffect = DeathEffect.instantiate()
+	deathEffect.global_position = global_position
+	get_parent().add_child(deathEffect)
+
+func take_damage():
+	Stats.player_hp -= 1
+	if Stats.player_hp == 0:
+		createDeathEffect()
+		sprite.visible = false
+		Stats.player_hp = Stats.START_HP
+		timer.start(2)
+	else:
+		createHitEffect()
 
 func _on_playehitarea_body_entered(body):
-	print("CHARACTER HIT")
 	if body.is_in_group("enemy_arrow"):
 		body.queue_free()
-		print("enem_arrow in player")
-		Stats.player_hp -= 1
-		print(Stats.player_hp)
-		if Stats.player_hp == 0:
-			print("hp ended, scene reset")
-			Stats.player_hp = Stats.START_HP
-			get_tree().reload_current_scene()
-	pass # Replace with function body.
+		take_damage()
+
+func _on_playehitarea_area_entered(area):
+	take_damage()
+
+func _on_timer_timeout():
+	sprite.visible = true
+	get_tree().reload_current_scene()
