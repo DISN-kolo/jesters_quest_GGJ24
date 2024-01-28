eb4026577c0c042d4dffefd53cfd186ec9ab517c
akozin <arrtemsky@gmail.com>
2024-01-28 14:58:12 +0100

    level11, platforming only

--- Items/hat_pickup.gd
+++ Items/hat_pickup.gd
@@ -3,12 +3,10 @@ extends Area2D
 var can_pickup = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if Stats.has_hat:
+		queue_free()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
 func _on_body_entered(body):
 	if body.is_in_group("player") and can_pickup:
@@ -20,4 +18,3 @@ func _on_body_entered(body):
 
 func _on_audio_stream_player_finished():
 	self.queue_free()
-	pass # Replace with function body.
